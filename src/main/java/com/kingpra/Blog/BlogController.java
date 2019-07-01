4c5212080be236b5b634f244f75cadc372720886
King Pra <kingi.pra@gmail.com>
2019-06-30 20:00:59 -0500

    removed boostrap

--- src/main/java/com/kingpra/Blog/BlogController.java
+++ src/main/java/com/kingpra/Blog/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class BlogController {
@@ -15,13 +16,38 @@ public class BlogController {
 	private BlogRepository blogRepo;
 
 	@GetMapping("/")
-	public String getIndexPage() {
+	// this displays the index page
+	public String getIndexPage(Model model) {
+		// this populates the model with all data from the database. Our key to access
+		// this data is "blogs". "blogs" is what we use for thymeleaf th:each
+		model.addAttribute("blogs", blogRepo.findAll());
+		// we return our index.html page and inject "blogs" into the view.
 		return "index";
 	}
 
 	@GetMapping("/new")
+	// method for getting new.html page
 	public String getNewPage() {
+		// returns new.html page
 		return "new";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String getEditPage(@PathVariable Long id, Model model, Blog blog) {
+		Blog post = blogRepo.findById(id).orElse(null);
+		model.addAttribute("blog", post);
+		return "edit";
+	}
+
+	@PutMapping("/edit/{id}")
+	public String editPost(@PathVariable Long id, Model model, Blog blog) {
+		Blog post = blogRepo.findById(id).orElse(null);
+		post.setAuthor(blog.getAuthor());
+		post.setTitle(blog.getTitle());
+		post.setContent(blog.getContent());
+		blogRepo.save(post);
+		model.addAttribute("blogs", blogRepo.findAll());
+		return "index";
 	}
 
 	@PostMapping("/new")
